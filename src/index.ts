2326c13fcefe6e59c036bac4b6fb2f8fceed7483
nbogie <neillbogie@googlemail.com>
2022-10-02 21:26:24 +0100

    update from the codesandbox

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,59 @@
-import { assign, createMachine, interpret } from "xstate";
+import { createMachine, assign, interpret } from "xstate";
 
-function isPlayValid(ctxt: any, event: any) {
-    console.log({ msg: "isPlayValid", ctxt, event });
-    return event.card.number > 5; //Math.random() > 0.5;
+//diagram here:
+//https://stately.ai/registry/editor/share/924d97fb-2bed-4892-a731-e5b6b5c3b49d
+interface CardGameContext {
+    cardsPlayed: Card[];
 }
 
-function isEndOfRound(ctxt: any, event: any) {
-    return Math.random() > 0.5;
-}
-function addPlayToContext(ctxt: any, event: any) {
-    console.log({ msg: "adding play to ctxt", ctxt, event });
-}
 interface Card {
     number: number;
 }
-/** @xstate-layout N4IgpgJg5mDOIC5QGECGAnCAFANqgngLKoDGAFgJYB2YATAHQCCA7qhQC7VS4EDEADnnxpMiUPwD2sDhQlUxIAB6IAjADYALPQ0B2NQFYAzAA41AThUAGHbVsAaEPlUbL9M4bVqd+ryuOHLQ0MAX2CHEWwhYnJqOnoANVQcCghUTiooCN4FSWlOOQVlBA01Bg1aFVozM311Mw19MwcnBH0GQ1qDK0sVQz8q0PCMSIJoyhoGLHQJEjhpDKycqRkCpCVEY1odeiqqnXLLKpdaZudXd09vX39AkMGQKgkIOAUIniJScbiWNnTuISWeVk8jWRRUGjM2jMOl6hloXksNUMpwQtmM9GM3S8tAa8Ksxnubyin1iDESyVSfwigJWINARXhkMsEJsNhK-mRjg2WjUQUs5n0RnBtA8hOG7zGpPoUxmcy41LWuVphUQBi0MMsbR0-JMjSaXIQKm1bkq+h0xg0HP0xn2YswEpJEyYrEwXAA6tQafk6etDYcVPQdIZ9hp1Cpw9Z9S0VDUMWZeT1eiozT07SMPjEJl7gSrDbR9FCYUF4dqkSoUQBaY3Wln80yIipmUKhIA */
-const machine = createMachine({
+
+type PlayCardEvent = {
+    type: "playCard";
+    card: Card;
+};
+
+function isPlayValid(ctxt: any, event: any, meta: any) {
+    console.log({
+        msg: "isPlayValid",
+        ctxt,
+        event,
+        meta: meta.state.event,
+    });
+    //QUESTION: is it ok to get the event this way?
+    //I think "always" transition guards get no event.
+    return meta.state.event.card.number <= 3;
+}
+
+const addCardToContext = assign({
+    cardsPlayed: (ctx: CardGameContext, event: PlayCardEvent, meta) => {
+        console.log("in addCardToContext: ", { ctx, event, meta });
+
+        //Question: how can we get provide access to the originating event from before an always transition?
+        //Should we manually propagate it from the preceding state, ValidatingCard?
+        // throw new Error("no card available to addCardToContext action");
+        if (!meta || !meta.state) {
+            throw new Error(
+                "no meta  or else no meta.state in addCardToContext"
+            );
+        }
+        const cardToAdd: Card = meta.state.event.card; //this won't work, currently
+        console.log("I HAD IT", cardToAdd);
+        // Alternative for testing - just add a random card.
+        // const cardToAdd: Card = { number: Math.round(Math.random() * 10) };
+        return [...ctx.cardsPlayed, cardToAdd];
+    },
+});
+
+function isEndOfRound(ctxt: CardGameContext, event: any) {
+    return ctxt.cardsPlayed.length >= 3;
+}
+
+export const cardGameMachine = createMachine<CardGameContext, PlayCardEvent>({
     context: { cardsPlayed: [] as Card[] },
-    predictableActionArguments: true,
-    entry: () => console.log("entered main machine"),
+    predictableActionArguments: false, //all hinges on this
     id: "CardPlayMachine2",
     initial: "AwaitingPlay",
     states: {
@@ -41,7 +76,7 @@ const machine = createMachine({
             ],
         },
         ProcessingCard: {
-            entry: addPlayToContext,
+            entry: addCardToContext,
             always: [
                 {
                     cond: isEndOfRound,
@@ -58,7 +93,11 @@ const machine = createMachine({
     },
 });
 
-const service = interpret(machine);
+const service = interpret(cardGameMachine);
 service.start();
-const exampleCardPlayEvent = { type: "playCard", card: { number: 7 } };
+const exampleCardPlayEvent: PlayCardEvent = {
+    type: "playCard",
+    card: { number: 2 },
+};
 service.send(exampleCardPlayEvent);
+console.log(service.getSnapshot().context);
