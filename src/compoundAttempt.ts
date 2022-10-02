5ed812d7b29107d3833fac54c8308d6af72f61cf
nbogie <neillbogie@googlemail.com>
2022-10-02 21:00:43 +0100

    add a compound soln attempt

--- src/compoundAttempt.ts
+++ src/compoundAttempt.ts
@@ -1,15 +1,20 @@
 import { createMachine, interpret } from "xstate";
 
-function isPlayValid(ctxt: any, event: any) {
-    console.log({ msg: "isPlayValid", ctxt, event });
-    return event.card.number > 5; //Math.random() > 0.5;
+function isPlayValid(ctxt: any, event: any, meta: any) {
+    console.log({
+        msg: "isPlayValid",
+        ctxt,
+        event,
+        meta: meta.state.event,
+    });
+    return meta.state.event.card.number > 5; //Math.random() > 0.5;
 }
 
 function isEndOfRound(ctxt: any, event: any) {
     return Math.random() > 0.5;
 }
-function addPlayToContext(ctxt: any, event: any) {
-    console.log({ msg: "adding play to ctxt", ctxt, event });
+function addCardToContext(ctxt: any, event: any, meta: any) {
+    console.log({ msg: "adding play to ctxt", ctxt, event, meta });
 }
 interface Card {
     number: number;
@@ -34,7 +39,7 @@ const machine = createMachine({
                 ValidatingCard: {
                     always: [
                         {
-                            cond: "playIsValid",
+                            cond: isPlayValid,
                             target: "IncorporatingCard",
                         },
                         {
@@ -43,10 +48,10 @@ const machine = createMachine({
                     ],
                 },
                 IncorporatingCard: {
-                    entry: "addCardToContext",
+                    entry: addCardToContext,
                     always: [
                         {
-                            cond: "endOfRound",
+                            cond: isEndOfRound,
                             target: "#CardPlayMachine3.AwardingWin",
                         },
                         {
@@ -66,3 +71,4 @@ const service = interpret(machine);
 service.start();
 const exampleCardPlayEvent = { type: "playCard", card: { number: 7 } };
 service.send(exampleCardPlayEvent);
+console.log(service.getSnapshot().context);
